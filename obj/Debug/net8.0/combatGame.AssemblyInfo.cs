94f7af49e9abad88b5f36a1d8cee4583ce925c10
loziamina02 <loziamina02@gmail.com>
2026-05-28 16:18:07 +0200

    creer de la factory des ennemi

--- obj/Debug/net8.0/combatGame.AssemblyInfo.cs
+++ obj/Debug/net8.0/combatGame.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("combatGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df2eed3c862a6d4d44944e1572fe613c0bbe1894")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f897ff30ac519c201b315d08d6725850a83680f")]
 [assembly: System.Reflection.AssemblyProductAttribute("combatGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("combatGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
