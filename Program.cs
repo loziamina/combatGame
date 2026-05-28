84e919e4952b7a139c4cf6603277d69eb87c6b53
loziamina02 <loziamina02@gmail.com>
2026-05-28 15:05:40 +0200

    ajout des 3 vagues de combat

--- Program.cs
+++ Program.cs
@@ -55,120 +55,163 @@ else if (nomClasse == "Voleur")
 Console.WriteLine("PV : " + pvHero);
 Console.WriteLine("Attaque : " + attaqueHero);
 
-string nomEnnemi = "Gobelin";
-int pvEnnemi = 40;
-int attaqueEnnemi = 8;
-int soinsRestants = 2;
+int numeroVague = 1;
+int totalVagues = 3;
 int cooldown = 0;
 
-Console.WriteLine("Un " + nomEnnemi + " apparaît !");
-Console.WriteLine(nomEnnemi + " | PV : " + pvEnnemi + " | Attaque : " + attaqueEnnemi);
-
-while (pvHero > 0 && pvEnnemi > 0)
+// boucle des vagues
+while (numeroVague <= totalVagues && pvHero > 0)
 {
-  // Reduit le cooldown au début de chaque tour
-  if (cooldown > 0)
-    cooldown = cooldown - 1;
+  string nomEnnemi = "";
+  int pvEnnemi = 0;
+  int attaqueEnnemi = 0;
+  int soinsRestants = 2;
 
-  Console.WriteLine("--- Votre tour ---");
-  Console.WriteLine(heroName + " | PV : " + pvHero);
-  Console.WriteLine(nomEnnemi + " | PV : " + pvEnnemi);
-
-  // Menu d'actions
-  Console.WriteLine("Que voulez-vous faire ?");
-  Console.WriteLine("1. Attaquer");
-  Console.WriteLine("2. Se soigner (+25 PV) - reste : " + soinsRestants);
-
-  // Affiche la compétence selon la classe et le cooldown
-  if (cooldown > 0)
-    Console.WriteLine("3. Compétence (recharge dans " + cooldown + " tour(s))");
-  else if (nomClasse == "Guerrier")
-    Console.WriteLine("3. Frappe Lourde (dégâts x1.5)");
-  else if (nomClasse == "Mage")
-    Console.WriteLine("3. Éclair (ignore 50% armure)");
-  else if (nomClasse == "Voleur")
-    Console.WriteLine("3. Coup Critique (30% chance x2)");
-
-  Console.Write("Votre choix (1/2/3) : ");
-  string choixAction = Console.ReadLine();
-
-  while (choixAction != "1" && choixAction != "2" && choixAction != "3")
+  if (numeroVague == 1)
   {
-    Console.WriteLine("Choix invalide ! Tapez 1, 2 ou 3.");
-    Console.Write("Votre choix (1/2/3) : ");
-    choixAction = Console.ReadLine();
+    nomEnnemi = "Gobelin";
+    pvEnnemi = 40;
+    attaqueEnnemi = 8;
+  }
+  else if (numeroVague == 2)
+  {
+    nomEnnemi = "Gobelin Archer";
+    pvEnnemi = 35;
+    attaqueEnnemi = 11;
+  }
+  else if (numeroVague == 3)
+  {
+    nomEnnemi = "Boss Orc";
+    pvEnnemi = 150;
+    attaqueEnnemi = 22;
   }
 
-  if (choixAction == "1")
+  Console.WriteLine("=== VAGUE " + numeroVague + "/" + totalVagues + " ===");
+  Console.WriteLine("Un " + nomEnnemi + " apparaît !");
+  Console.WriteLine(nomEnnemi + " | PV : " + pvEnnemi + " | Attaque : " + attaqueEnnemi);
+
+  while (pvHero > 0 && pvEnnemi > 0)
   {
-    // Attaque normale
-    pvEnnemi = pvEnnemi - attaqueHero;
-    Console.WriteLine("Vous attaquez le " + nomEnnemi + " ! Il perd " + attaqueHero + " PV.");
-  }
-  else if (choixAction == "2")
-  {
-    // Soin
-    if (soinsRestants > 0)
-    {
-      pvHero = pvHero + 25;
-      soinsRestants = soinsRestants - 1;
-      Console.WriteLine("Vous vous soignez de 25 PV ! PV actuels : " + pvHero + " | Soins restants : " + soinsRestants);
-    }
-    else
-    {
-      Console.WriteLine("Plus de soins disponibles !");
-    }
-  }
-  else if (choixAction == "3")
-  {
-    // definir les compétences pour chaque classes
+    // Reduit le cooldown au début de chaque tour
     if (cooldown > 0)
-    {
-      Console.WriteLine("Compétence pas encore disponible !");
-    }
+      cooldown = cooldown - 1;
+
+    Console.WriteLine("--- Votre tour ---");
+    Console.WriteLine(heroName + " | PV : " + pvHero);
+    Console.WriteLine(nomEnnemi + " | PV : " + pvEnnemi);
+
+    // Menu d'actions
+    Console.WriteLine("Que voulez-vous faire ?");
+    Console.WriteLine("1. Attaquer");
+    Console.WriteLine("2. Se soigner (+25 PV) - reste : " + soinsRestants);
+
+    // Affiche la compétence selon la classe et le cooldown
+    if (cooldown > 0)
+      Console.WriteLine("3. Compétence (recharge dans " + cooldown + " tour(s))");
     else if (nomClasse == "Guerrier")
-    {
-      int degats = (int)(attaqueHero * 1.5);
-      pvEnnemi = pvEnnemi - degats;
-      cooldown = 2;
-      Console.WriteLine("Frappe Lourde ! Vous infligez " + degats + " dégâts !");
-    }
+      Console.WriteLine("3. Frappe Lourde (dégâts x1.5)");
     else if (nomClasse == "Mage")
-    {
-      int degats = attaqueHero + 10;
-      pvEnnemi = pvEnnemi - degats;
-      cooldown = 3;
-      Console.WriteLine("Éclair ! Vous infligez " + degats + " dégâts magiques !");
-    }
+      Console.WriteLine("3. Éclair (ignore 50% armure)");
     else if (nomClasse == "Voleur")
+      Console.WriteLine("3. Coup Critique (30% chance x2)");
+
+    Console.Write("Votre choix (1/2/3) : ");
+    string choixAction = Console.ReadLine();
+
+    while (choixAction != "1" && choixAction != "2" && choixAction != "3")
     {
-      Random aleatoire = new Random();
-      int tirage = aleatoire.Next(1, 101);
-      if (tirage <= 30)
+      Console.WriteLine("Choix invalide ! Tapez 1, 2 ou 3.");
+      Console.Write("Votre choix (1/2/3) : ");
+      choixAction = Console.ReadLine();
+    }
+
+    if (choixAction == "1")
+    {
+      // Attaque normale
+      pvEnnemi = pvEnnemi - attaqueHero;
+      Console.WriteLine("Vous attaquez le " + nomEnnemi + " ! Il perd " + attaqueHero + " PV.");
+    }
+    else if (choixAction == "2")
+    {
+      // Soin
+      if (soinsRestants > 0)
       {
-        int degats = attaqueHero * 2;
-        pvEnnemi = pvEnnemi - degats;
-        Console.WriteLine("COUP CRITIQUE ! Vous infligez " + degats + " dégâts !");
+        pvHero = pvHero + 25;
+        soinsRestants = soinsRestants - 1;
+        Console.WriteLine("Vous vous soignez de 25 PV ! PV actuels : " + pvHero + " | Soins restants : " + soinsRestants);
       }
       else
       {
-        pvEnnemi = pvEnnemi - attaqueHero;
-        Console.WriteLine("Pas de critique... Vous infligez " + attaqueHero + " dégâts.");
+        Console.WriteLine("Plus de soins disponibles !");
       }
-      cooldown = 2;
+    }
+    else if (choixAction == "3")
+    {
+      // definir les compétences pour chaque classes
+      if (cooldown > 0)
+      {
+        Console.WriteLine("Compétence pas encore disponible !");
+      }
+      else if (nomClasse == "Guerrier")
+      {
+        int degats = (int)(attaqueHero * 1.5);
+        pvEnnemi = pvEnnemi - degats;
+        cooldown = 2;
+        Console.WriteLine("Frappe Lourde ! Vous infligez " + degats + " dégâts !");
+      }
+      else if (nomClasse == "Mage")
+      {
+        int degats = attaqueHero + 10;
+        pvEnnemi = pvEnnemi - degats;
+        cooldown = 3;
+        Console.WriteLine("Éclair ! Vous infligez " + degats + " dégâts magiques !");
+      }
+      else if (nomClasse == "Voleur")
+      {
+        Random aleatoire = new Random();
+        int tirage = aleatoire.Next(1, 101);
+        if (tirage <= 30)
+        {
+          int degats = attaqueHero * 2;
+          pvEnnemi = pvEnnemi - degats;
+          Console.WriteLine("COUP CRITIQUE ! Vous infligez " + degats + " dégâts !");
+        }
+        else
+        {
+          pvEnnemi = pvEnnemi - attaqueHero;
+          Console.WriteLine("Pas de critique... Vous infligez " + attaqueHero + " dégâts.");
+        }
+        cooldown = 2;
+      }
+    }
+
+    // Lennemi attaque le hero
+    if (pvEnnemi > 0)
+    {
+      pvHero = pvHero - attaqueEnnemi;
+      Console.WriteLine(nomEnnemi + " attaque " + heroName + " ! Vous perdez " + attaqueEnnemi + " PV.");
     }
   }
 
-  // Lennemi attaque le hero
-  if (pvEnnemi > 0)
+  // fin de la vague
+  if (pvHero > 0)
   {
-    pvHero = pvHero - attaqueEnnemi;
-    Console.WriteLine(nomEnnemi + " attaque " + heroName + " ! Vous perdez " + attaqueEnnemi + " PV.");
+    Console.WriteLine("Vague " + numeroVague + " terminée !");
+
+    // recupere 20% des PV entre les vagues
+    if (numeroVague < totalVagues)
+    {
+      int pvRestores = (int)(pvHero * 0.20);
+      pvHero = pvHero + pvRestores;
+      Console.WriteLine("Vous récupérez " + pvRestores + " PV ! PV actuels : " + pvHero);
+    }
+
+    numeroVague = numeroVague + 1;
   }
 }
 
 // affiche le résultat du combat
 if (pvHero > 0)
-  Console.WriteLine("Vous avez vaincu le " + nomEnnemi + " !");
+  Console.WriteLine("VICTOIRE ! Vous avez vaincu toutes les vagues !");
 else
   Console.WriteLine("Vous avez été vaincu...");
